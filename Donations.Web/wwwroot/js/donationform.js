9e49ba432ecd188466303e32b7644653b90befc1
Jared Bacik <jb@jaredbacik.com>
2021-06-01 00:44:48 -0400

    Currency.js to cleanup client values

--- Donations.Web/wwwroot/js/donationform.js
+++ Donations.Web/wwwroot/js/donationform.js
@@ -5,21 +5,47 @@ $(() => {
         const isCoveringCosts = $('#IsCoveringCosts').prop('checked');
         const amount = $(this).val();
 
-        if (isNaN(amount) || Number(amount) <= 0.0) {
+        tryUpdateTotals(amount, isCoveringCosts);
+    });
+
+    $('#IsCoveringCosts').on('change', function () {
+        const isCoveringCosts = $(this).prop('checked');
+        const amount = $('#DonationAmount').val();
+
+        tryUpdateTotals(amount, isCoveringCosts);
+    });
+
+    function tryUpdateTotals(amount, isCoveringCosts) {
+        if (isNaN(amount)) {
             $('#CoveredCosts').text('');
             $('#TotalDonationAmount').text('');
+            return;
         }
 
-        if (isCoveringCosts && amount > 0) {
-            fetch(`/api/feecalculator?amount=${amount}`)
+        if (isCoveringCosts) {
+            $('#CoveredCostsContainer').show();
+            getTotalWithCoveredCost(amount);
+        } else {
+            $('#CoveredCosts').text('');
+            $('#TotalDonationAmount').text('$' + currency(amount));
+            $('#CoveredCostsContainer').hide();
+        }
+    }
+
+    function getTotalWithCoveredCost(amount) {
+        const value = currency(amount);
+
+        if (value > 0) {
+            fetch(`/api/feecalculator?amount=${value}`)
                 .then(response => response.json())
                 .then(data => {
-                    $('#CoveredCosts').text('$' + data);
-                    const total = data + Number(amount);
+                    $('#CoveredCosts').text('$' + currency(data));
+                    const total = currency(data).add(value);
                     $('#TotalDonationAmount').text('$' + total);
                 });
         } else {
-            $('#TotalDonationAmount').text('$' + amount);
+            $('#CoveredCosts').text('');
+            $('#TotalDonationAmount').text('$' + value);
         }
-    });
+    }
 });
